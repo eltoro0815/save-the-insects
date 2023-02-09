cb911a2fe4c489d229f3d9c2cc0981cf2d294678
eltoro0815 <th_ru@web.de>
2023-02-09 15:46:42 +0100

    initial commit

---
+++ app/Models/Insect.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Insect
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $type
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @property int $position
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect sorted()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Insect wherePosition($value)
+ */
+class Insect extends Model
+{
+    use \Rutorika\Sortable\SortableTrait;
+}
---
+++ resources/js/components/InsectComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="col d-flex">
+      <div class="row border align-items-baseline mt-4 ml-0 mr-0 bg-white">
+          <img
+                  :src="image"
+                  :alt="name | properCase"
+                  style="width:100%;max-width:400px;"
+                  class="p-2 mx-auto d-block my-handle align-self-baseline"
+          >
+
+          <h3 class="container-fluid text-center align-self-baseline">
+              <span>{{ name | properCase }}</span>
+          </h3>
+
+          <div class="align-self-end container-fluid">
+              <select class="custom-select mb-1" @change="update_insect_type">
+                  <option
+                          v-for="typ in [
+                                          'ant',
+                                          'bee',
+                                          'bug',
+                                          'butterfly',
+                                          'dragonfly',
+                                          'fly',
+                                          'grasshopper',
+                                          'ladybug',
+                                          'mantis',
+                                          'mosquito',
+                                          'roach',
+                                          'tick'
+                                      ]"
+                          :value="typ"
+                          :key="typ"
+                          :selected="typ === type ? 'selected' : ''"
+                  >{{ typ | properCase }}
+                  </option>
+              </select>
+              <button type="button" class="mb-1 btn btn-primary btn-block" @click="delete_insect">Delete</button>
+          </div>
+      </div>
+  </div>
+</template>
+<script>
+  export default {
+      computed: {
+          image() {
+              return `/images/insects/${this.type}_400x400.png`;
+          }
+      },
+      methods: {
+          update_insect_type(val) {
+              this.$emit("update_insect_type", this.id, val.target.selectedOptions[0].value);
+          },
+          delete_insect() {
+              this.$emit("delete_insect", this.id);
+          }
+      },
+      props: ["id", "type", "name"],
+      filters: {
+          properCase(string) {
+              return string.charAt(0).toUpperCase() + string.slice(1);
+          }
+      }
+  };
+</script>
